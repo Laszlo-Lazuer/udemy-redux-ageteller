949d27e77d1c4918e624f152f02994064f3d1d56
A. Laszlo Lazuer <visualjazz@gmail.com>
2021-05-08 16:22:38 -0500

    chore(stub): add bootstrap and form elems

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './components/App';
 
 ReactDom.render(
-<div>ageteller</div>, document.getElementById('root')
+<App />, document.getElementById('root')
 )
