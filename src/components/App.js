2ccc55ae9be2296ced8561bdc6ec7e9494e79130
A. Laszlo Lazuer <visualjazz@gmail.com>
2021-05-09 01:09:55 -0500

    chore(css): add css animation

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,9 @@ class App extends Component {
                     { ' ' }
                     <Button onClick={() => this.changeBirthday()}> Submit </Button>
                     {this.state.showStats &&
+                        <div className={'fade age-stats'}>
                         <AgeStats date={this.state.birthday} />
+                        </div>
                     }
                 </Form>
             </div>
